55a387f650a15d2cb2477a3001ec97b18d40b0bc
Samuel Adelakun <93160015+nukimoya@users.noreply.github.com>
2025-04-11 05:58:36 -0700

    database restructuting

--- config/database.js
+++ config/database.js
@@ -10,16 +10,16 @@ if (!databaseUrl) {
   // You might want to throw an error or provide a fallback
 }
 
-const sequelize = new Sequelize(databaseUrl, {
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // Needed for some PostgreSQL providers
-    }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT
   }
-});
+);
 
 // Self-invoking function to test connection
 (async () => {
